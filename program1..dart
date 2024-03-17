bd508a4fe92ab74515180d5ef40596893e599948
AlanGitau <lewisgitau99@gmail.com>
2024-03-18 02:41:47 +0300

    First hackathon

--- program1..dart
+++ program1..dart
@@ -1,2 +1,10 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//## Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+var name="Alan";
+var age= 21;
+var school="Kca university";
+var Hobby='swimming';
+
+void main(){
+print("My Name is $name am $age years  old and I study at $school. I like going $Hobby with my friends");
+}
